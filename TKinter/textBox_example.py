506d9b3b74b53edae56fb0b644ce84db6a0af54f
sltm-14 <ie693242@iteso.mx>
2020-10-09 23:47:36 -0500

    text box example

--- TKinter/textBox_example.py
+++ TKinter/textBox_example.py
@@ -2,7 +2,7 @@ import tkinter # only import tkinr
 
 def textoDeEntrada():
     texto = cajaTexto.get()
-    print(texto)
+    etiqueta["text"] =texto
 
 ventana = tkinter.Tk()
 ventana.geometry("500x300")
@@ -10,6 +10,8 @@ ventana.geometry("500x300")
 cajaTexto = tkinter.Entry(ventana,font = "Helvetica 20") #ventana en la que se muestra, fuente tamaño de letra
 cajaTexto.pack()
 
+etiqueta = tkinter.Label(ventana)
+etiqueta.pack()
 
 boton = tkinter.Button(ventana,text = "click",command = textoDeEntrada)
 boton.pack()
